21f95dd117e82788fc8be02c4a207c9792bebfc1
salleeb <saliem.brhane@chasacademy.se>
2023-04-07 12:40:54 +0200

    .

--- RecipeAPISearch/src/app/app.component.ts
+++ RecipeAPISearch/src/app/app.component.ts
@@ -1,38 +1,47 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
 import { RecipeService } from './recipe-api/recipe.service';
-import { FormControl,FormGroup,Validators,FormBuilder } from '@angular/forms';
+
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-
-  registerForm:any = FormGroup;
-submitted = false;
-constructor( private formBuilder: FormBuilder){}
-//Add user form actions
-get f() { return this.registerForm.controls; }
-onSubmit() {
   
-  this.submitted = true;
-  // stop here if form is invalid
-  if (this.registerForm.invalid) {
+  registerForm: any = FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) {}
+  //Add user form actions
+  get f() {
+    return this.registerForm.controls;
+  }
+  onSubmit() {
+    this.submitted = true;
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
       return;
+    }
+    //True if all the fields are filled
+    if (this.submitted) {
+      alert('Great!!');
+    }
+    console.log(this.submitted);
   }
-  //True if all the fields are filled
-  if(this.submitted)
-  {
-    alert("Great!!");
-  }
- 
-}
   ngOnInit() {
     //Add User form validations
     this.registerForm = this.formBuilder.group({
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required]]
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]],
     });
   }
 }
